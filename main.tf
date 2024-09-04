48a389420353dc86c729a43f169cf6c9100f7ee9
NaveenNkay <147308581+NaveenNkay@users.noreply.github.com>
2024-09-04 11:23:40 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "google_compute_network" "vpc_network" {
+name = "terraform-nk-VPC"
+}
+resource "google_compute_subnetwork" "subnetwork1" {
+name = "terraform-subnetwork-1"
+ip_cidr_range = "10.1.0.0/16"
+region = "us-central1"
+network = google_compute_network.vpc_network.name
+}
